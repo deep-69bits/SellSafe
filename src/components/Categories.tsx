2be42573613b0771f097ab5e138cbb848f5b9c67
deep-69bits <lcb2021004@iiitl.ac.in>
2024-01-16 17:54:14 +0530

    slider added

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,33 +1,108 @@
 import React from 'react'
+import '../app/slider.css'
 
 
 
-const CategoryAtom=({children}:any)=>{
-    return(
-        <div className='inline-block hover:scale-110 duration-500 transition-all cursor-pointer border-[1px] text-xl border-black bg-purple-200 rounded-full px-8 font-semibold py-3'>
-            {children}
-        </div>
-    )
+const CategoryAtom = ({ children }: any) => {
+  return (
+    <div className='inline-block hover:scale-110 duration-500 transition-all cursor-pointer border-[1px] text-xl border-black bg-purple-200 rounded-full px-8 font-semibold py-3'>
+      {children}
+    </div>
+  )
 }
 
 const Categories = () => {
   return (
-    <div className='lg:px-10 py-10 px-4 overflow-hidden hidden lg:flex flex-col gap-10 my-20'>
-        <div className='lg:flex  gap-x-8 lg:translate-x-40 '>
-                <CategoryAtom>MOBILES</CategoryAtom>
-                <CategoryAtom>ELECTRONICS</CategoryAtom>
-                <CategoryAtom>HOME DECOR</CategoryAtom>
-                <CategoryAtom>GAMING</CategoryAtom>
-                <CategoryAtom>MUSIC</CategoryAtom>
-                <CategoryAtom>AUTOMOBILE</CategoryAtom>
+    <div className='lg:px-10  px-4 overflow-hidden  flex flex-col gap-10 my-20'>
+
+      <div className="slider">
+        <div className="slide-track py-1">
+          <div className="slide">
+            <CategoryAtom>MOBILES</CategoryAtom>
+          </div>
+          <div className="slide">
+            <CategoryAtom>ELECTRONICS</CategoryAtom>
+          </div>
+          <div className="slide">
+            <CategoryAtom>HOME DECOR</CategoryAtom>
+          </div>
+          <div className="slide">
+            <CategoryAtom>GAMING</CategoryAtom>
+          </div>
+          <div className="slide">
+            <CategoryAtom>MUSIC</CategoryAtom>
+          </div>
+          <div className="slide">
+            <CategoryAtom>AUTOMOBILE</CategoryAtom>
+          </div>
+          <div className="slide">
+            <CategoryAtom>MOBILES</CategoryAtom>
+          </div>
+          <div className="slide">
+            <CategoryAtom>ELECTRONICS</CategoryAtom>
+          </div>
+          <div className="slide">
+            <CategoryAtom>HOME DECOR</CategoryAtom>
+          </div>
+          <div className="slide">
+            <CategoryAtom>GAMING</CategoryAtom>
+          </div>
+          <div className="slide">
+            <CategoryAtom>MUSIC</CategoryAtom>
+          </div>
+          <div className="slide">
+            <CategoryAtom>AUTOMOBILE</CategoryAtom>
+          </div>
+          <div className="slide">
+            <CategoryAtom>ELECTRONICS</CategoryAtom>
+          </div>
         </div>
-        <div className='flex gap-x-8 translate-x-10 '>
-                <CategoryAtom>HOME CARE</CategoryAtom>
-                <CategoryAtom>TWO WHEELERS</CategoryAtom>
-                <CategoryAtom>MOBILE ACCESSROIES</CategoryAtom>
-                <CategoryAtom>BABY CARE</CategoryAtom>
-                <CategoryAtom>LAPTOPS</CategoryAtom>
+      </div>
+
+
+      <div className="slider">
+        <div className="slide-track py-1">
+          <div className="slide">
+          <CategoryAtom>HOME CARE</CategoryAtom>
+          </div>
+          <div className="slide">
+          <CategoryAtom>TWO WHEELERS</CategoryAtom>
+          </div>
+          <div className="slide">
+          <CategoryAtom>BABY CARE</CategoryAtom>
+          </div>
+          <div className="slide">
+          <CategoryAtom>LAPTOPS</CategoryAtom>
+          </div>
+          <div className="slide">
+          <CategoryAtom>HOME CARE</CategoryAtom>
+          </div>
+          <div className="slide">
+          <CategoryAtom>TWO WHEELERS</CategoryAtom>
+          </div>
+          <div className="slide">
+          <CategoryAtom>BABY CARE</CategoryAtom>
+          </div>
+          <div className="slide">
+            <CategoryAtom>GAMING</CategoryAtom>
+          </div>
+          <div className="slide">
+          <CategoryAtom>LAPTOPS</CategoryAtom>
+          </div>
+          <div className="slide">
+          <CategoryAtom>TWO WHEELERS</CategoryAtom>
+          </div>
+          <div className="slide">
+          <CategoryAtom>HOME CARE</CategoryAtom>
+          </div>
+          <div className="slide">
+          <CategoryAtom>TWO WHEELERS</CategoryAtom>
+          </div>
+          <div className="slide">
+            <CategoryAtom>ELECTRONICS</CategoryAtom>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
