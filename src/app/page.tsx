b06079629ab12feaaca5b815458b31e8d08318e1
deep-69bits <lcb2021004@iiitl.ac.in>
2024-01-28 04:16:45 +0530

    button sticky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,25 @@ import Products from '@/components/Products'
 import Categories from '@/components/Categories'
 import StoreSection from '@/components/StoreSection'
 import Footer from '@/components/Layout/Footer'
-
+import Image from 'next/image'
+import './slider.css'
+import FooterTop from '@/components/Layout/FooterTop'
 
 export default function Home() {
   return (
     <main className="">
-    <Hero/> 
-    <Products/>
-    <Categories/> 
-    <Feature/>
-    <Faq/>
-    <Contact/>
-    <Footer/>
-    {/* <StoreSection/> */}
+      
+        <Hero />
+        <Products />
+        <Categories />
+        <Feature />
+        <Faq />
+        <Contact />
+        <FooterTop/>
+        <div className=' flex z-50  sticking   w-full  lg:translate-y-[-50px]     flex-row justify-center '>
+          <Image src={'/Images/GooglePlay.svg'} width={180} height={68} alt='Google Play Button' />
+          <Image src={'/Images/AppleStore.svg'} width={180} height={68} alt='Apple Store Button' />
+        </div>
     </main>
   )
 }
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({children}:any) => {
     <div className='selection:bg-transparent max-w-[1800px] mx-auto min-h-screen  bg-purple-100 bg-opacity-30'>
        <Header/>
           {children}
-        
+       <Footer/>
     </div>
   )
 }
